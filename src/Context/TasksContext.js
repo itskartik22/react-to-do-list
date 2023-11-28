47cccb51e928e41d539f716c21360ba62c6d907e
Kartik Thakur <thakurkartik2262@gmail.com>
2023-11-28 18:59:31 +0530

    unnecessary context commented

--- src/Context/TasksContext.js
+++ src/Context/TasksContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
 export const TasksContext = createContext(null);
-export const TasksDispatchContext = createContext(null)
+// export const TasksDispatchContext = createContext(null)
 export const ViewContext = createContext(null)
 export const ViewDispatchContext = createContext(null)
